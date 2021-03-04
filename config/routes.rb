1d9957a3d75aeafa168b6c6c1fed258569371540
Eula Gonzales <eulabgm@gmail.com>
2021-03-04 17:04:31 +0100

    Mister Cocktail Draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :show, :create] do
-    resources :doses, only: [:new, :create]
+  resources :cocktails, only: %w[index new show create] do
+    resources :doses, only: %w[new create]
   end
-
-  resources :doses, only: [:destroy]
+  resources :doses, only: ['destroy']
 end
